ef67cc4cb68bea417214ea04d81d14a4129bf534
avery finchum <finchum.avery@gmail.com>
2022-05-16 11:38:00 -0400

    initial main commit

---
+++ PictulrMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PictulrMVC.Startup))]
+namespace PictulrMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
